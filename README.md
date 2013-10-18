d0fb13c14246407fbada0e947b11a6753e73671e
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:02:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+readline Cookbook
+=================
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "readline"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures readline"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "readline", "Installs/Configures readline"
+
+supports "suse"
